59d1ba472562d297731ca715278e12200a3f68ef
vivek <durishettivivek@gmail.com>
2025-06-06 18:34:31 +0530

    Update testimonials section layout

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -26,9 +26,18 @@ const Testimonials: React.FC = () => {
           </p>
         </div>
 
-        <div className="max-w-4xl mx-auto">
-          <FeedbackForm />
-          <FeedbackList />
+        <div className="max-w-6xl mx-auto">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+            {/* Left Side - Latest Feedback */}
+            <div className="order-2 lg:order-1">
+              <FeedbackList />
+            </div>
+            
+            {/* Right Side - Share Your Experience */}
+            <div className="order-1 lg:order-2">
+              <FeedbackForm />
+            </div>
+          </div>
         </div>
       </div>
     </section>
