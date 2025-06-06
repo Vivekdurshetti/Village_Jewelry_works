734cbb95273e143166360429d5a268a3a9f2f0c2
vivek <durishettivivek@gmail.com>
2025-06-06 18:16:32 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -66,7 +66,7 @@ const Footer: React.FC = () => {
               <p className="flex items-start mb-3">
                 <MapPin className="w-5 h-5 mr-2 text-gold-500 mt-1" />
                 <a
-                  href="https://www.google.com/maps?q=Beside+RCM+shop,+Basar+Village,+Nirmal+District,+Telangana+State"
+                  href="https://maps.app.goo.gl/FhBNtvxDxpXSWpLn8"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="hover:text-gold-500 transition-colors"
